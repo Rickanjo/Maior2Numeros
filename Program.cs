9cdf6dd921bb28107ba322663e3a36e1408c7daf
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-24 19:25:58 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Receba dois números. Exiba o maior.
+Console.WriteLine("--- Exibindo o maior número ---");
+
+Console.Write("Digite o primeiro número...: ");
+int numero1 = int.Parse(Console.ReadLine()!);
+        
+Console.Write("Digite o segundo número....: ");
+int numero2 = int.Parse(Console.ReadLine()!);
+
+if (numero1 > numero2) {
+    Console.WriteLine($"\nO maior número é...........: {numero1}");
+
+} else if (numero1 < numero2) {
+    Console.WriteLine($"\nO maior número é...........: {numero2}");
+
+}else {
+    Console.WriteLine("\nOs números são iguais");
+}
+
